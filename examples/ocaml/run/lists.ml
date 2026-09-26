3cedcb4b2b6b9a5b92bec58be86619a28080dbf1
brent hartshorn <brenthartshorn@proton.me>
2026-09-25 21:23:29 -0700

    Ocaml to Rust to LEAN4 - part VI - lists

--- examples/ocaml/run/lists.ml
+++ examples/ocaml/run/lists.ml
@@ -1,6 +1,6 @@
 (* lists: recursion down a list, [@@variant l] its size *)
 let rec length l = match l with [] -> 0 | _ :: t -> 1 + length t
-  [@@ensures result >= 0] [@@variant l]
+  [@@ensures result >= 0 && result <= List.length l] [@@variant l]
 let rec sum l = match l with [] -> 0 | x :: t -> x + sum t
   [@@variant l]
 let () = print_int (length [1; 2; 3]); print_int (sum [4; 5]); print_newline ()
